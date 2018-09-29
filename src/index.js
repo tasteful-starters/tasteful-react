75f2e134ee966ee1d2706d285c02a3da2d128ae0
coiso <buesimples@gmail.com>
2018-09-29 13:17:01 +0100

    step 4

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import env from '@env';
-
-const App = () =>
-    console.log({env}) ||
-    (
-        <div>
-            <h1>Hello, world!</h1>
-        </div>
-    );
+import App from './app';
 
 ReactDOM.render(<App />, document.getElementById("root"));
